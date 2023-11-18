53dd5e5efe5c139890aedc01658e889d3ba8093a
cpprian <cpprian456@gmail.com>
2023-11-18 12:39:59 +0100

    add wallet handler in settings

---
+++ lib/screens/import_wallet.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:d_allegro/providers/wallet_provider.dart';
+
+class ImportWallet extends StatefulWidget {
+  const ImportWallet({Key? key}) : super(key: key);
+
+  @override
+  _ImportWalletState createState() => _ImportWalletState();
+}
+
+class _ImportWalletState extends State<ImportWallet> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
