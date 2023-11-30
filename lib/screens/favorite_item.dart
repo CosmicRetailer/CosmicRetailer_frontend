662f76328b68c1d25584e9ba61f4283cc2f3fcc2
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-11-30 17:47:24 +0100

    Make favorite

---
+++ lib/screens/favorite_item.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:d_allegro/http_client.dart';
+import 'product_page.dart'; // Import DescriptionPage
+
+class FavoriteItemsPage extends StatefulWidget {
+  const FavoriteItemsPage({Key? key});
+
+  @override
+  _FavoriteItemsPageState createState() => _FavoriteItemsPageState();
+}
+
+class _FavoriteItemsPageState extends State<FavoriteItemsPage> {
+  late Future<List<dynamic>?> favoriteItems;
+
+  @override
+  void initState() {
+    super.initState();
+    favoriteItems = fetchFavoriteItems();
+  }
+
+  Future<List<dynamic>?> fetchFavoriteItems() async {
+    final response = await dio.get('$apiURL/get_favorites');
+
+    if (response.statusCode == 200) {
+      return response.data['favorites'];
+    } else {
+      throw Exception('Failed to load favorite items');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista Ulubionych Przedmiotów'),
+      ),
+      body: FutureBuilder<List<dynamic>?>(
+        future: favoriteItems,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final favoriteItems = snapshot.data;
+
+            if (favoriteItems != null && favoriteItems.isNotEmpty) {
+              return ListView.builder(
+                itemCount: favoriteItems.length,
+                itemBuilder: (context, index) {
+                  final item = favoriteItems[index];
+                  final title = item['name'];
+                  final price = item['price'] != null
+                      ? double.parse(item['price'].toString())
+                      : 0.0;
+
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DescriptionPage(
+                            arguments: ProductPageArguments(
+                              item[
+                                  '_id'], // Assuming id is the unique identifier of the item
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    child: Column(
+                      children: [
+                        Image.network(
+                          item['photoUrl'] ?? 'https://picsum.photos/200',
+                          height: 200,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(
+                          title,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          'Price: \$${price.toStringAsFixed(2)}',
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            } else {
+              return const Center(child: Text('No favorite items found.'));
+            }
+          } else {
+            return const Center(child: Text('No favorite items available.'));
+          }
+        },
+      ),
+    );
+  }
+}
